c5a134e76f427492031f1af3ab3c8f7796d90ec0
141CJ <141cj+prod@proton.me>
2026-09-05 14:13:11 -0400

    feat: add arg to decode images into ascii

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use clap::{Parser, ValueEnum};
-use opencv::core::{self, MatExprTraitConst, MatTrait};
+use opencv::core::{self, MatExprTraitConst, MatTrait, MatTraitConst};
 use opencv::imgcodecs;
 
 #[derive(PartialEq, Copy, Clone, Debug, ValueEnum)]
@@ -32,8 +32,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     match args.mode {
         Mode::Encode => encode(args.text)?,
-        Mode::Decode => todo!(),
+        Mode::Decode => decode(args.text)?,
     };
+    Ok(())
+}
+
+fn decode(img_name: String) -> Result<(), Box<dyn std::error::Error>> {
+    let img = imgcodecs::imread(img_name, imgcodecs::IMREAD_COLOR)?;
+    let size = img.rows() * img.cols();
+    for i in 0..size {
+        let color = img.at::<core::Vec3b>(i).unwrap();
+        print!(
+            "{}{}{}",
+            color[2] as char, color[1] as char, color[0] as char
+        );
+    }
     Ok(())
 }
 
@@ -52,18 +65,18 @@ fn encode(text: String) -> Result<(), Box<dyn std::error::Error>> {
         colors.push(i as u8);
         if colors.len() == 3 {
             let pixel = img.at_mut::<core::Vec3b>(index)?;
-            pixel[0] = colors[0];
+            pixel[2] = colors[0];
             pixel[1] = colors[1];
-            pixel[2] = colors[2];
+            pixel[0] = colors[2];
             colors.clear();
             index += 1;
         }
     }
     if !colors.is_empty() {
         let pixel = img.at_mut::<core::Vec3b>(index)?;
-        pixel[0] = colors.get(0).copied().unwrap_or(0);
+        pixel[2] = colors.get(0).copied().unwrap_or(0);
         pixel[1] = colors.get(1).copied().unwrap_or(0);
-        pixel[2] = colors.get(2).copied().unwrap_or(0);
+        pixel[0] = colors.get(2).copied().unwrap_or(0);
     }
 
     imgcodecs::imwrite("pixels.png", &img, &core::Vector::new())?;
